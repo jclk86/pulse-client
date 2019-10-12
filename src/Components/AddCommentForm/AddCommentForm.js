2b8bdc7454180eba0472c08010aa75e39fe623b6
John Chan <johnchancontact@gmail.com>
2019-10-12 09:39:00 -0400

    add comment form

---
+++ src/Components/AddCommentForm/AddCommentForm.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import { Form } from "../Utils/Utils"
+
+class AddCommentForm extends Component { 
+  render() {
+    return (
+    <Form>
+      
+    </Form>)
+  }
+}
