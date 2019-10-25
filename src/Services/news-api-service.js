82e9441458b3106bf03d7752070b150434bf0d91
John Chan <johnchancontact@gmail.com>
2019-10-25 14:38:33 -0400

    removed header from news api request

--- src/Services/news-api-service.js
+++ src/Services/news-api-service.js
@@ -3,13 +3,7 @@ import config from "../config";
 const NewsApiService = {
   getNews() {
     return fetch(`${config.NEWS_ENDPOINT}`, {
-      method: "GET",
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers":
-          "Origin, X-Requested-With, Content-Type, Accept",
-        "Access-Control-Allow-Credentials": "true"
-      }
+      method: "GET"
     }).then(res =>
       !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
     );
