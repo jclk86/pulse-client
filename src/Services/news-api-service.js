b967629dd693f8ab457de0e698e0552f414879aa
John Chan <johnchancontact@gmail.com>
2019-10-25 14:34:20 -0400

    fixing news api cors error

--- src/Services/news-api-service.js
+++ src/Services/news-api-service.js
@@ -5,7 +5,10 @@ const NewsApiService = {
     return fetch(`${config.NEWS_ENDPOINT}`, {
       method: "GET",
       headers: {
-        "content-type": "application/json"
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers":
+          "Origin, X-Requested-With, Content-Type, Accept",
+        "Access-Control-Allow-Credentials": "true"
       }
     }).then(res =>
       !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
