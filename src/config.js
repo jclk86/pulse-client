71cac4f0110495823120333be15111b9a10216c0
John Chan <johnchancontact@gmail.com>
2019-10-24 21:26:03 -0400

    changed API ENDPOINT

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-  API_ENDPOINT: "http://localhost:8000/api", //"https://arcane-refuge-94724.herokuapp.com/api",
+  // API_ENDPOINT: "http://localhost:8000/api",
+  API_ENDPOINT: "https://arcane-refuge-94724.herokuapp.com/api",
   TOKEN_KEY: process.env.REACT_APP_API_KEY,
   GEOLOCATION_ENDPOINT: `https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.REACT_APP_GEOLOCATOR_API_KEY}`
 };
