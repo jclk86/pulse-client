104c8463b5bb5eb9250f0447b55f040fc6679328
John Chan <johnchancontact@gmail.com>
2019-11-09 11:04:32 -0500

    fix favicon

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   // API_ENDPOINT: "http://localhost:8000/api",
-  API_ENDPOINT: " https://arcane-refuge-94724.herokuapp.com/api",
+  API_ENDPOINT: "https://arcane-refuge-94724.herokuapp.com/api",
   TOKEN_KEY: process.env.REACT_APP_API_KEY,
   NEWS_ENDPOINT: `https://newsapi.org/v2/everything?q=traveling&sortBy=publishedAt&pageSize=15&apiKey=afc5d75ddfff4597be88b995978ccdab`
 };
