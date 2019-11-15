969604d923fe6872c03b0f0588c7e19cc015335c
John Chan <johnchancontact@gmail.com>
2019-11-15 13:49:41 -0500

    fixing cors issue

--- src/Services/auth-api-service.js
+++ src/Services/auth-api-service.js
@@ -6,10 +6,10 @@ const AuthApiService = {
   postLogin({ username, password }) {
     return fetch(`${config.API_ENDPOINT}/auth/login`, {
       method: "POST",
+      body: JSON.stringify({ username, password }),
       headers: {
         "content-type": "application/json"
-      },
-      body: JSON.stringify({ username, password })
+      }
     })
       .then(res =>
         !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
@@ -27,10 +27,10 @@ const AuthApiService = {
   postUser(user) {
     return fetch(`${config.API_ENDPOINT}/user`, {
       method: "POST",
+      body: JSON.stringify(user),
       headers: {
         "content-type": "application/json"
-      },
-      body: JSON.stringify(user)
+      }
     }).then(res =>
       !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
     );
