fd831a95b202a4f419e63bfee39bf2a6bbf80708
John Chan <johnchancontact@gmail.com>
2020-06-11 10:48:31 -0400

    fixed ticker

--- src/config.js
+++ src/config.js
@@ -1,6 +1,23 @@
+let currentDate = Date.now();
+
+function formatDate(date, offset=0) {
+  var d = new Date(date),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate() - offset,
+    year = d.getFullYear();
+
+  if (month.length < 2)
+    month = '0' + month;
+  if (day.length < 2)
+    day = '0' + day;
+
+  return [year, month, day].join('-');
+}
+
+
 export default {
   // API_ENDPOINT: "http://localhost:8000/api",
   API_ENDPOINT: "https://arcane-refuge-94724.herokuapp.com/api",
   TOKEN_KEY: process.env.REACT_APP_API_KEY,
-  NEWS_ENDPOINT: `https://newsapi.org/v2/everything?q=traveling&sortBy=publishedAt&pageSize=15&apiKey=afc5d75ddfff4597be88b995978ccdab`
+  NEWS_ENDPOINT: `https://newsapi.org/v2/everything?q=traveling&sortBy=publishedAt&from=${formatDate(currentDate)}&to=${formatDate(currentDate, 3)}&pageSize=15&apiKey=afc5d75ddfff4597be88b995978ccdab`
 };
